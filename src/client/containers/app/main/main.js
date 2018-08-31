25c11c55b9b8e46130c9912dc0018751e49b1398
Jan WALSH <jwalsh@e3r7p20.42.fr>
2018-08-31 16:29:52 +0200

    cleaned up main.js

--- src/client/containers/app/main/main.js
+++ src/client/containers/app/main/main.js
@@ -3,40 +3,12 @@ import { connect } from 'react-redux'
 import styles from './main.css'
 import PlayerForm from '../../../components/playerForm/playerForm'
 import HostList from '../../../components/hostList/hostList'
-
 import socket from '../../../socket'
-
 import {updatePlayerName, updateSelectedGame} from '../../../actions/client'
 
 // import * as ActionNames from '../../../../server/serverActions'
 
 const Main = ( props ) => {
-
-	// const onUpdateName = () => {
-	// 	// console.log("selectGame: ", hostID);
-	//
-	// 	// socket.emit('action', {
-	// 	// 	hostID: hostID,
-	// 	// 	playerName: 'playerName'
-	// 	// });
-	//
-	// 	props.onUpdatePlayerName(document.getElementById('playerInputName').value);
-	// 	console.log("PlayerName: ", props.playerName);
-	//
-	// 	if (props.playerName != null && props.playerName.length > 0) {
-	//
-	// 		console.log("Emit new player");
-	// 		socket.emit(ActionNames.NEW_PLAYER, props.playerName);
-	// 	}
-	// 	else {
-	// 		//Error
-	// 	}
-	//
-	// }
-
-	// if (props.playerName.length > 0)
-	// 	socket.emit(ActionNames.NEW_PLAYER, props.playerName);
-
 	let content;
 
 	if (props.playerName === undefined || props.playerName.length == 0) {
